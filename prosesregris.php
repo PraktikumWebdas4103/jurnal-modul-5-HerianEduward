03c171ad205bc61a284dabfdcae25a0e80bef7ef
HerianEduward <43520686+HerianEduward@users.noreply.github.com>
2018-10-02 16:59:03 +0700

    Update prosesregris.php

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,22 @@
 <?php
-
-
-?>
+ $hostname   = "localhost";
+ $username = "root";
+ $pass     = "";
+ $db       = "pendaftaranmahasiswa";
+ $con     = new mysqli($hostname, $username, $pass, $db);
+ if($con==false){
+  die("koneksi gagal". $con->connect_error);
+ }
+  if (isset($_POST['submit'])) {
+   $NIM=$_POST['nim'];
+   $Nama=$_POST['nama'];
+   $Email=$_POST['email'];
+ 
+ $sql="INSERT INTO pendaftaranmahasiswa(nim,nama,email) VALUES ('$nim','$nama','$email')";
+   if(mysqli_query($con,$sql)){
+    echo "Database sudah masuk";
+   }else{
+    echo "error";;
+   }
+}
+ ?>
