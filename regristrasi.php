a3e4082c79bf0f8ef3caba5324223cfd7b7500a0
HerianEduward <43520686+HerianEduward@users.noreply.github.com>
2018-10-02 16:59:28 +0700

    Update regristrasi.php

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,38 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
+<form action="koneksi.php " method="post">
+<center><h1>Pendaftaran Mahasiswa</h1>
+	<table>
+		<tr>
+			<td>NIM</td>
+			<td><input type="text" name="nim" placeholder="NIM" maxlength="10"></td>
+		</tr>
+		<tr>
+			<td>Nama</td>
+			<td><input type="text" name="nama" placeholder="Nama" maxlength="25"></td>
+		</tr>
+		<tr>
+			<td>E-mail</td>
+			<td><input type="E-mail" name="email" value="@gmail.com"></td>
+		</tr>
+		<tr>
+		</tr>
+			<td><input type="submit" name="submit" value="Login"></td>
+	</table>
+</center>
 
-
-
-</body>
-</html>
+<?php
+if (isset($_POST['submit'])) {
+   $nim=$_POST['nim'];
+   $nama=$_POST['nama'];
+   $email=$_POST['email'];
+   $Error = array();
+   if (strlen($_POST['nim'])<=10) {
+    $error['nim']=="nim harus 10";
+   }if(strlen($_POST['nama'])<=25){
+    $error['nama']=="nama harus 25";
+   }if (empty($email)) {
+    $error['email']=="email tidak boleh kosong";
+  
+   }
+   include 'koneksi.php';
+}
+?>
